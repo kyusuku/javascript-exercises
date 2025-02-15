6d53712b8dd2e4357473880825f79bc2b280dfd8
kyusuku <147250847+kyusuku@users.noreply.github.com>
2025-02-15 16:58:19 +0700

    Complete 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, reps) {
+    if (reps < 0) {
+        return "ERROR";
+    }
 
+    let ans = "";
+
+    for (let i = 0; i < reps; i++) {
+        ans += string;
+    }
+
+    return ans;
 };
 
 // Do not edit below this line
