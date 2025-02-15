77832041949d2ca11df38d11479e21a8971c69d8
kyusuku <147250847+kyusuku@users.noreply.github.com>
2025-02-15 18:02:04 +0700

    Complete 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || 
+        !Number.isInteger(b)) {
+        return "ERROR";
+    }
 
+    let max = a > b ? a : b;
+    let min = a < b ? a : b;
+
+    let sum = 0;
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
