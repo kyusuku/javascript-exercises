2191f4faa7734fc15da7fa1ffc8ff55144303ace
kyusuku <147250847+kyusuku@users.noreply.github.com>
2025-02-21 22:41:03 +0700

    Complete 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let res = people[0];
+    const currentYear = (new Date()).getFullYear();
+    let lastAge = ("yearOfDeath" in res) ? res.yearOfDeath - res.yearOfBirth :
+        currentYear - res.yearOfBirth;
+    
+    for (let i = 1; i < people.length; i++) {
+        let person = people[i];
 
+        currAge = ("yearOfDeath" in person) ? person.yearOfDeath - person.yearOfBirth :
+        currentYear - person.yearOfBirth;
+
+        res = currAge > lastAge ? person : res;
+        lastAge = currAge;
+    }
+
+    return res;
 };
 
 // Do not edit below this line
