8aae89dc6176b81a22a4bcf13b7da22aef3aa78b
kyusuku <147250847+kyusuku@users.noreply.github.com>
2025-02-21 21:50:25 +0700

    Complete 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let arr = [0, 1];
 
-};
+    for (let i = 2; i <= n; i++) {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+
+    return n >= 0 ? arr[n] : "OOPS";
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
