49111f309833faac66e2dca5ed741545d9cb233f
kyusuku <147250847+kyusuku@users.noreply.github.com>
2025-02-21 21:42:16 +0700

    Complete 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    const filteredString = string
+        .toLowerCase()
+        .split("")
+        .filter(char => alphanumerical.includes(char))
+        .join("");
+
+    const reverseString = filteredString.split("").reverse().join("");
+
+    return filteredString === reverseString;
 };
 
 // Do not edit below this line
