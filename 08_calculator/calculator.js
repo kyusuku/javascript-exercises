78c2d839d541c54a519a50262e984120d4a8419b
kyusuku <147250847+kyusuku@users.noreply.github.com>
2025-02-21 16:44:31 +0700

    Complete 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, curr) => total + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, curr) => total * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0 || n === 1) return 1;
+  else {
+    let res = new Array(n + 1);
+    res[0] = 1;
+    res[1] = 1;
+
+    for (let i = 2; i <= n; i++) {
+      res[i] = res[i - 1] * i;
+    }
+
+    return res[n];
+  }
 };
 
 // Do not edit below this line
